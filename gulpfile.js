cb35ba1f100eb7b3d88622a4e2f35b03f5c1ba65
Tad Fisher <tad@simple.com>
2017-04-11 17:12:46 -0700

    Minify; add favicons

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,50 @@
 'use strict';
 
-var gulp = require('gulp'),
-    append = require('gulp-append'),
+var append = require('gulp-append'),
     autoprefixer = require('gulp-autoprefixer'),
-    cleanCss = require('gulp-clean-css'),
+    cheerio = require('cheerio'),
+    CleanCSS = require('clean-css'),
+    connect = require('gulp-connect'),
+    del = require('del'),
     exec = require('child_process').exec,
+    htmlmin = require('gulp-htmlmin'),
+    gulp = require('gulp'),
+    map = require('map-stream'),
     replace = require('gulp-replace'),
+    rx = require('rxjs/Rx'),
+    rxToStream = require('rxjs-stream').rxToStream,
+    streamToRx = require('rxjs-stream').streamToRx,
     sass = require('gulp-sass'),
+    uncss = require('uncss'),
+    watch = require('gulp-watch'),
+
+    html = {
+      src: './public/**/*.html',
+      dest: './public'
+    },
 
     styles = {
       src: './src/scss/*.scss',
-      dest: './layouts/partials/stylesheet.html'
+      dest: './build/styles.css'
     };
 
+function hugo(args) {
+  const cmd = ['hugo', args].join(' ');
+  return function(done) {
+    exec(cmd, function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      done(err);
+    });
+  };
+}
+
+gulp.task('clean', function(done) {
+  del(['./public/**/*', '!./public', '!./public/.gitkeep']).then(paths => done());
+});
+
 gulp.task('build:styles', function () {
-  gulp.src(styles.src)
+  return gulp.src(styles.src)
 
     .pipe(sass({
       includePaths: ['./node_modules'],
@@ -23,32 +53,70 @@ gulp.task('build:styles', function () {
 
     .pipe(replace('!important'))
 
-    .pipe(cleanCss({level: 2}))
-
     .pipe(append(styles.dest));
 });
 
-gulp.task('build:drafts', function (cb) {
-  exec('hugo --buildDrafts --buildFuture', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('hugo:drafts', hugo('--buildDrafts --buildFuture'));
+
+gulp.task('hugo:public', hugo());
+
+gulp.task('minify', function () {
+  var rxuncss = rx.Observable.bindNodeCallback(uncss, (output, error) => output);
+
+  var cleancss = new CleanCSS();
+
+  return gulp.src(html.src)
+
+    .pipe(map((file, cb) => rx.Observable.of(file.contents.toString())
+      .concatMap(html => streamToRx(gulp.src(styles.dest))
+        .map(file => file.contents.toString())
+        .concatMap(css => rxuncss(html, { ignoreSheets: [/\s*/], raw: css }))
+        .map(output => {
+          var $ = cheerio.load(html);
+          $('head > style[amp-custom]').text(output);
+          return $.html();
+        }))
+      .subscribe(output => {
+        file.contents = new Buffer(output, 'utf8');
+        cb(null, file);
+      })))
+
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      minifyCss: true,
+      minifyJs: true
+    }))
+
+    .pipe(gulp.dest(html.dest));
 });
 
-gulp.task('build:public', ['build:styles'], function (cb) {
-  exec('hugo', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('build:dev', gulp.series('clean', 'build:styles', 'hugo:drafts', 'minify'));
+
+gulp.task('build:prod', gulp.series('clean', 'build:styles', 'hugo:public', 'minify'));
+
+gulp.task('watch:dev', function() {
+  gulp.watch(['./content/**/*', './data/**/*', './layouts/**/*', './static/**/*', './themes/**/*'],
+             { ignored: 'layouts/partials/stylesheet.html', ignoreInitial: false }, gulp.series('build:dev'));
 });
 
-gulp.task('serve', function (cb) {
-  gulp.watch(styles.src, ['build:styles']);
-  exec('hugo server --buildDrafts --buildFuture --bind=0.0.0.0', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+gulp.task('serve:dev', function() {
+  connect.server({
+    root: 'public',
+    livereload: true
   });
+
+  gulp.src('./public/**/*')
+    .pipe(watch('./public/**/*'), { ignoreInitial: false })
+    .pipe(connect.reload());
+});
+
+gulp.task('serve:prod', function() {
+  connect.server({
+    root: 'public',
+    livereload: false
+  });
+
+  gulp.src('./public/**/*')
+    .pipe(watch('./public/**/*'), { ignoreInitial: false })
+    .pipe(connect.reload());
 });
